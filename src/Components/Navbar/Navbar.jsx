40a6b8b68c86bb7edce25e6f33b0b688d4192fd7
m2kung <melanikung@gmail.com>
2025-04-30 15:06:24 -0400

    added blog

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import underline from '../../assets/nav_underline.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import menu_open from '../../assets/menu_open.svg'
 import menu_close from '../../assets/menu_close.svg'
+import { Link } from 'react-router'
 
 const Navbar = () => {
 
@@ -25,11 +26,12 @@ const Navbar = () => {
         <img src={menu_open} onClick={openMenu} alt="" className='nav-mob-open' />
         <ul ref={menuRef} className="nav-menu">
             <img src={menu_close} onClick={closeMenu} alt="" className="nav-mob-close" />
-            <li><AnchorLink className='anchor-link' href='#home'><p onClick={() => setMenu("home")}>Home</p></AnchorLink>{menu === "home" ? <img src={underline} alt="" /> : <></>}</li>
-            <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={() => setMenu("about")}>About Me</p></AnchorLink>{menu === "about" ? <img src={underline} alt="" /> : <></>}</li>
-            <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={() => setMenu("services")}>Services</p></AnchorLink>{menu === "services" ? <img src={underline} alt="" /> : <></>}</li>
-            <li><AnchorLink className='anchor-link' offset={50} href='#work'><p onClick={() => setMenu("work")}>Portfolio</p></AnchorLink>{menu === "work" ? <img src={underline} alt="" /> : <></>}</li>
-            <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={() => setMenu("contact")}>Contact</p></AnchorLink>{menu === "contact" ? <img src={underline} alt="" /> : <></>}</li>
+            <li><AnchorLink className='anchor-link' href='#home'><p onClick={() => setMenu("home")}>home</p></AnchorLink>{menu === "home" ? <img src={underline} alt="" /> : <></>}</li>
+            <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={() => setMenu("about")}>about</p></AnchorLink>{menu === "about" ? <img src={underline} alt="" /> : <></>}</li>
+            <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={() => setMenu("services")}>my work</p></AnchorLink>{menu === "services" ? <img src={underline} alt="" /> : <></>}</li>
+            <li><AnchorLink className='anchor-link' offset={50} href='#work'><p onClick={() => setMenu("work")}>experience</p></AnchorLink>{menu === "work" ? <img src={underline} alt="" /> : <></>}</li>
+            <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={() => setMenu("contact")}>contact</p></AnchorLink>{menu === "contact" ? <img src={underline} alt="" /> : <></>}</li>
+            <li><a className='anchor-link' href="https://medium.com/@melanikung" target="_blank" rel="noopener noreferrer">my stuff</a></li>
         </ul>
         <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href='#contact' onClick={() => setMenu("contact")}>Connect With Me</AnchorLink></div>
     </div>
