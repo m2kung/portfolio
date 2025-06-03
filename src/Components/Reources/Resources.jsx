191c56bfc64c7ebb9ca5e61f2d0b8adf8a6eead7
m2kung <melanikung@gmail.com>
2025-06-03 18:39:36 -0400

    need to fix resources

--- src/Components/Reources/Resources.jsx
+++ src/Components/Reources/Resources.jsx
@@ -6,31 +6,28 @@ import arrow_icon from '../../assets/arrow_icon.svg'
 
 const Projectstest = () => {
 
-    const [isHovered, setIsHovered] = useState(false);
-
   return (
     <div className='mywork' id='work'>
         <div className="mywork-title">
             <h1>resources & rec's</h1>
             <img src={theme_pattern} alt="" />
         </div>
-        <div className="mywork-container"
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}>
-                    {isHovered ? 
-                    <div className="work-container">{mywork_data.map((work,index) =>
-                      {const nextItem = mywork_data[index + 1];
-                        const firstItem = mywork_data[0]
-                        return nextItem ? (
-                        <img src={nextItem.w_img} alt="" />
-                      ) : <img src={firstItem.w_img} alt="" />;
-                    })} </div>: 
-                    <div className="work-container">{mywork_data.map((work,index)=>{return <img src={work.w_img} alt="" />})}</div>}
-        </div>
-        <div className="mywork-showmore">
+    <div className="mywork-container">
+        <div className="work-inner">
+          <div className="work-front">
+            {mywork_data.map((work,index)=>{
+              return <img key={index} src={work.w_img} alt="" />
+            })}
             <p>Show More</p>
             <img src={arrow_icon} alt="" />
+        <div className="mywork-showmore">
         </div>
+        </div>
+        <div className="work-back">
+            I have been crucified with Christ. It is no longer I who live, but Christ who lives in me. And the life I now live in the flesh I live by faith in the Son of God, who loved me and gave himself for me.
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
