1a5487a9b993b6db48d23dd89f1fc5b6ac1fa733
m2kung <melanikung@gmail.com>
2025-06-07 15:38:04 -0400

    small formatting changes. need to fix resources

--- src/Components/Reources/Resources.jsx
+++ src/Components/Reources/Resources.jsx
@@ -8,27 +8,20 @@ const Projectstest = () => {
 
   return (
     <div className='mywork' id='work'>
-        <div className="mywork-title">
-            <h1>resources & rec's</h1>
-            <img src={theme_pattern} alt="" />
-        </div>
-    <div className="mywork-container">
-        <div className="work-inner">
-          <div className="work-front">
-            {mywork_data.map((work,index)=>{
-              return <img key={index} src={work.w_img} alt="" />
-            })}
+      <div className="mywork-title">
+          <h1>resources & rec's</h1>
+          <img src={theme_pattern} alt="" />
+      </div>
+      <div className="mywork-container">
+          {mywork_data.map((work,index)=>{
+            return <img key={index} src={work.w_img} alt="" />
+          })}
+        <div className="mywork-showmore">
             <p>Show More</p>
             <img src={arrow_icon} alt="" />
-        <div className="mywork-showmore">
-        </div>
-        </div>
-        <div className="work-back">
-            I have been crucified with Christ. It is no longer I who live, but Christ who lives in me. And the life I now live in the flesh I live by faith in the Son of God, who loved me and gave himself for me.
-        </div>
         </div>
       </div>
-    </div>
+    </div> 
   )
 }
 
