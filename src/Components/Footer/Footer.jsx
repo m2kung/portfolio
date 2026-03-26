3fc46c5cd98798f3f4421f442f583426a00b407a
m2kung <melanikung@gmail.com>
2026-03-26 19:57:14 -0400

    edits to footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -21,10 +21,12 @@ const Footer = () => {
         </div>
         <hr />
         <div className="footer-bottom">
-            <p className="footer-bottom-left">✞<br></br>soli deo gloria</p>
+            <div className="footer-bottom-left">
+                <p><i>My flesh and my heart may fail, <br></br>
+                    but <span>God is the strength of my heart</span> and my portion forever.</i> <br />~Ps. 76:23 ESV</p>
+            </div>
             <div className="footer-bottom-right">
-                <p>My flesh and my heart may fail, <br></br>
-                    but <span>God is the strength of my heart</span> and my portion forever. <br />~Ps. 76:23 ESV</p>
+                <p>✞<br></br>soli deo gloria</p>
             </div>
         </div>
     </div>
